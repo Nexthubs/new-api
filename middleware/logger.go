f66cd5bd498ade85b1387dc7697d7d509b4916c2
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2025-07-14 14:47:54 +0800

    Update logger.go

--- middleware/logger.go
+++ middleware/logger.go
@@ -1,25 +1,98 @@
 package middleware
 
 import (
+	"bytes"
+	"compress/gzip"
 	"fmt"
-	"github.com/gin-gonic/gin"
+	"io"
 	"one-api/common"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
+const (
+	maxSize = 1024
+)
+
+func RequestLogger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if !common.ResponseLogEnabled || c.Request.Body == nil {
+			c.Next()
+			return
+		}
+		bodyBytes, err := common.GetRequestBody(c)
+		if err != nil {
+			common.SysError("error reading request body: " + err.Error())
+			c.Next()
+			return
+		}
+
+		if len(bodyBytes) > 0 {
+			bodyToLog := string(bodyBytes)
+			if len(bodyToLog) > maxSize {
+				bodyToLog = bodyToLog[:maxSize] + "..."
+			}
+			msg := fmt.Sprintf("%s %s %s\n%s",
+				c.ClientIP(),
+				c.Request.Method,
+				c.Request.URL.Path,
+				bodyToLog)
+			common.LogInfo(c.Request.Context(), msg)
+		}
+		c.Next()
+	}
+}
+
 func SetUpLogger(server *gin.Engine) {
+	server.Use(RequestLogger())
 	server.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
 		var requestID string
 		if param.Keys != nil {
 			requestID = param.Keys[common.RequestIdKey].(string)
 		}
-		return fmt.Sprintf("[GIN] %s | %s | %3d | %13v | %15s | %7s %s\n",
+
+		logStr := fmt.Sprintf("[GIN] %s | %s | %3d | %13v | %15s | %7s %s\n",
 			param.TimeStamp.Format("2006/01/02 - 15:04:05"),
 			requestID,
 			param.StatusCode,
-			param.Latency,
-			param.ClientIP,
+	@@ -21,5 +61,39 @@ func SetUpLogger(server *gin.Engine) {
 			param.Method,
 			param.Path,
 		)
+
+		logStr += responseLog(param)
+
+		return logStr
 	}))
+
+	// add a middleware to log the response body when gin detail enabled
+	server.Use(func(c *gin.Context) {
+		c = common.WrapWriter(c)
+		c.Next()
+	})
+}
+
+func responseLog(param gin.LogFormatterParams) string {
+	if !common.DebugEnabled {
+		return ""
+	}
+	blw, ok := param.Keys[common.KeyResponseWriter].(common.BodyLogWriter)
+	if !ok {
+		return ""
+	}
+	body := blw.String()
+	// decompressed gzip response body
+	if strings.Contains(param.Request.Header.Get("Accept-Encoding"), "gzip") {
+		if reader, err := gzip.NewReader(bytes.NewReader([]byte(body))); err == nil {
+			if decompressed, err := io.ReadAll(reader); err == nil {
+				body = string(decompressed)
+			}
+		}
+	}
+
+	if len(body) > maxSize {
+		body = string(body[:maxSize]) + "..."
+	}
+	return fmt.Sprintf("%s\n", body)
 }
